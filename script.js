0ecb678e9c16a65bbdb9aa4bc7b65f9be404d1d8
peterbuoy <peterbuoy@gmail.com>
2021-06-07 23:09:46 -0700

    created buttons and event listeners with feedback on the page

--- script.js
+++ script.js
@@ -1,38 +1,111 @@
 let hands = ['Rock', 'Paper', 'Scissors']
 let playerSelection = ''
 let playerScore = 0, computerScore = 0, round = 1
-game()
 
-function game() {
-	for (let i = 0; i < 5; i++) {
-	 	playerSelection = prompt("Rock, Paper, Scissors?")
-		console.log(`Round ${round++}`);
-		computerSelection = computerPlay()
-		console.log(playRound(playerSelection, computerSelection))
-		console.log(`Player: ${playerScore}, Computer: ${computerScore}`);
-		console.log();
-	}
-	determineResult(playerScore, computerScore)
-}
+const btn_rock = document.querySelector('#btn_rock');
+const btn_paper = document.querySelector('#btn_paper');
+const btn_scissors = document.querySelector('#btn_scissors');
+const btn_reset = document.querySelector('#btn_restart')
+btn_rock.addEventListener('click', () => {
+	playRound('Rock', computerPlay())
+});
+btn_paper.addEventListener('click', () => {
+	playRound('Paper', computerPlay())
+});
+btn_scissors.addEventListener('click', () => {
+	playRound('Scissors', computerPlay())
+});
+btn_restart.addEventListener('click', () => {
+	const restartButton = document.getElementById('btn_restart')
+	const result_container = document.querySelector('#result_container')
+	result_container.textContent = '';
+	playerScore = 0;
+	computerScore = 0;
+	restartButton.disabled = true;
+	const buttons = document.getElementsByClassName('button-hand');
+			for (let i = 0; i < buttons.length; i++) {
+				buttons[i].disabled = false;
+			}
+	
+});
+
+// game()
+
+// function game() {
+// 	for (let i = 0; i < 5; i++) {
+// 	 	playerSelection = prompt("Rock, Paper, Scissors?")
+// 		console.log(`Round ${round++}`);
+// 		computerSelection = computerPlay()
+// 		console.log(playRound(playerSelection, computerSelection))
+// 		console.log(`Player: ${playerScore}, Computer: ${computerScore}`);
+// 		console.log();
+// 	}
+// 	determineResult(playerScore, computerScore)
+// }
 
 function playRound(playerSelection, computerSelection) {
+	const result_container = document.querySelector('#result_container')
 	playerSelection = normalizePlayerSelection(playerSelection)
+	result_container.textContent = `You chose ${playerSelection} `;
+	result_container.textContent += `Computer chose ${computerSelection}`
 	console.log(`You chose ${playerSelection}`);
 	console.log(`Computer chose ${computerSelection}`)
 
+	// Tie
 	if (playerSelection == computerSelection) {
-		return `You tied because you both chose ${playerSelection}`
+		const resultOutcome = document.createElement('div');
+		resultOutcome.textContent = `You tied because you both chose ${playerSelection}`
+		result_container.appendChild(resultOutcome);
+		const resultScore = document.createElement('div');
+		resultScore.textContent = `Player score: ${playerScore} Computer score: ${computerScore}`
+		result_container.appendChild(resultScore);
 	} 
+	// Lose
 	else if (
 		(playerSelection == 'Rock' && computerSelection == 'Paper') || 
 		(playerSelection == 'Paper' && computerSelection == 'Scissors') ||
 		(playerSelection == 'Scissors' && computerSelection == 'Rock')
 		) {
-		computerScore++
+		const resultOutcome = document.createElement('div');
+		resultOutcome.textContent = `You lost! ${computerSelection} beats ${playerSelection}`;
+		result_container.appendChild(resultOutcome);
+		computerScore++;
+		const resultScore = document.createElement('div');
+		resultScore.textContent = `Player score: ${playerScore} Computer score: ${computerScore}`
+		result_container.appendChild(resultScore);
+		if (computerScore == 5) {
+			const winnerMessage = document.createElement('div');
+			winnerMessage.textContent = `Computer won the game!`
+			result_container.appendChild(winnerMessage)
+			const buttons = document.getElementsByClassName('button-hand');
+			for (let i = 0; i < buttons.length; i++) {
+				buttons[i].disabled = true;
+			}
+			const restartButton = document.getElementById('btn_restart');
+			restartButton.disabled = false;
+		}
 		return `You lost! ${computerSelection} beats ${playerSelection}`
 	} 
+	// Win
 	else {
 		playerScore++
+		const resultOutcome = document.createElement('div');
+		resultOutcome.textContent = `You won! ${playerSelection} beats ${computerSelection}`;
+		result_container.appendChild(resultOutcome);
+		const resultScore = document.createElement('div');
+		resultScore.textContent = `Player score: ${playerScore} Computer score: ${computerScore}`
+		result_container.appendChild(resultScore);
+		if (playerScore == 5) {
+			const winnerMessage = document.createElement('div');
+			winnerMessage.textContent = `You won the game!`
+			result_container.appendChild(winnerMessage)
+			const buttons = document.getElementsByClassName('button-hand');
+			for (let i = 0; i < buttons.length; i++) {
+				buttons[i].disabled = true;
+			}
+			const restartButton = document.getElementById('btn_restart');
+			restartButton.disabled = false;
+		}
 		return `You won! ${playerSelection} beats ${computerSelection}`
 	}
 }
